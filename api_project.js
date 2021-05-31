9dd6e7545d44c51c1aaf6f1e60b778f197af6d19
Amit Kaushik <kaushikabhi999@gmail.com>
2021-05-31 20:17:12 +0530

    Social Login API created and changed API naming styling

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-24T12:05:26.029Z",
+    "time": "2021-05-31T14:45:03.561Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
