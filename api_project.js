0684ca14b583b65d8084d4af937b1877ad8ba769
pukhraj saini <pukhraj@mobilecoderz.com>
2021-07-09 18:57:44 +0530

    property types and bed types api

--- api_project.js
+++ api_project.js
@@ -7,8 +7,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-08T07:25:54.146Z",
+    "time": "2021-07-09T13:19:43.802Z",
     "url": "https://apidocjs.com",
-    "version": "0.25.0"
+    "version": "0.28.1"
   }
 });
